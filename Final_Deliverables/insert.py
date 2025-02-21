ebe2754c1462c9a7dd1a80534523fd62d82ca60c
Ung <ungant67@students.rowan.edu>
2025-02-21 16:37:00 -0500

    Updated insret.py

--- Final_Deliverables/insert.py
+++ Final_Deliverables/insert.py
@@ -4,12 +4,15 @@ ORM Project
 Insert and Update
 
 Demonstrates inserting and then updating records.
+In addition to inserting and updating records, we have found ways to
+    catch excptions in Python and to commit/rollback transactions.
 '''
 
 import sqlalchemy as a
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.exc import SQLAlchemyError
+from mysql.connector.errors import IntegrityError
 
 
 def DEBUG_PRINT(message):
@@ -59,15 +62,24 @@ try:
 except SQLAlchemyError as e:
     session.rollback()
     print(f"Transaction rolled back due to error: {e}")
+    print(f"\nThis block would be reached if the insertion was already completed.\n\n")
 
 '''
 I lost my job as an Evaluation Assistant because the course instructor
     no longer has the enrollment numbers to qualify for having me.
 '''
+print('Updating an Employee record. This is idempotent and has been done before.\n')
 users = session.query(Employee).filter(Employee.lastName == 'Ung').filter(Employee.firstName == 'Anthony')
 users.update({Employee.jobTitle: 'Research Assistant'})
 session.commit()
 
+print('Updated employee record after my job got eliminated:\n')
+users = session.query(Employee).filter(Employee.lastName == 'Ung').filter(Employee.firstName == 'Anthony')
+for row in users.all():
+    print(row.__dict__)
+
+# prompt user to close then close session and engine
+yes = input("Press any key to close")
 
 #cleanup
 session.close()
