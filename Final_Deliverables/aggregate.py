89fde22cda48dee539cb6b669bf7320c0b3b488e
Ung <ungant67@students.rowan.edu>
2025-02-21 22:18:33 -0500

    Added code to do aggregation

---
+++ Final_Deliverables/aggregate.py
@@ -0,0 +1,73 @@
+'''
+Anthony Ung, Cory Lillis
+ORM Project
+Aggregate
+
+In addition to the functionality discussed in class, 
+    we demonstrate aggregation in Python's ORM.
+
+Our business would want to know which customers 
+    are making the biggest.
+'''
+
+# Necessary import statements
+import sqlalchemy as alch
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.exc import SQLAlchemyError
+from mysql.connector.errors import IntegrityError
+
+# A helpful debug method 
+def DEBUG_PRINT(message):
+    print(message)
+    yes = input('\n' 'Enter any string to exit.' '\n')
+
+# Checks for the connect script being in the same directory.
+# Separate becasue I do not want bots and scrapers to grab my login credentials from Github.
+try:
+    import connect
+except:
+    DEBUG_PRINT('Error!\n'\
+               'You do not have the appropriate\n'\
+               'connect.py file in the right directory\n')
+
+# Prepare the engine and session
+engine = Engine = connect.connect()
+Base = automap_base()
+Base.prepare(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Grabbing tables
+Order = Base.classes.orders
+OrderDetail = Base.classes.orderdetails
+Customer = Base.classes.customers
+Product = Base.classes.products
+ProductLine = Base.classes.productlines
+Employee = Base.classes.employees
+
+
+'''
+
+'''
+try:
+    stmt = alch.select(Customer.customerName, \
+                            alch.func.sum(OrderDetail.quantityOrdered * OrderDetail.priceEach).label('Total Purchases'))\
+                    .join(Order, Customer.customerNumber == Order.customerNumber)\
+                    .join(OrderDetail, Order.orderNumber == OrderDetail.orderNumber)\
+                    .group_by(Customer.customerNumber)\
+                    .order_by(alch.desc('Total Purchases'))\
+                    .limit(10)
+    for row in session.execute(stmt):
+        print(row)
+except SQLAlchemyError as e:
+    print(f"Transaction rolled back due to error: {e}")
+
+
+
+# prompt user to close then close session and engine
+yes = input("Press any key to close")
+
+#cleanup
+session.close()
+engine.dispose()
--- Final_Deliverables/insert.py
+++ Final_Deliverables/insert.py
@@ -8,18 +8,20 @@ In addition to inserting and updating records, we have found ways to
     catch excptions in Python and to commit/rollback transactions.
 '''
 
+# Necessary import statements
 import sqlalchemy as a
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.exc import SQLAlchemyError
 from mysql.connector.errors import IntegrityError
 
-
+# A helpful debug method 
 def DEBUG_PRINT(message):
     print(message)
     yes = input('\n' 'Enter any string to exit.' '\n')
 
-
+# Checks for the connect script being in the same directory.
+# Separate becasue I do not want bots and scrapers to grab my login credentials from Github.
 try:
     import connect
 except:
@@ -27,14 +29,14 @@ except:
                'You do not have the appropriate\n'\
                'connect.py file in the right directory\n')
 
-# login
+# Prepare the engine and session
 engine = Engine = connect.connect()
 Base = automap_base()
 Base.prepare(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# grabbing tables
+# Grabbing tables
 Order = Base.classes.orders
 OrderDetail = Base.classes.orderdetails
 Customer = Base.classes.customers
@@ -42,9 +44,12 @@ Product = Base.classes.products
 ProductLine = Base.classes.productlines
 Employee = Base.classes.employees
 
+
 '''
 I inserted myself into the table.
 reportsTo is NULLable because the President does not report to anybody.
+In addition to the parts of SQLAlchemy that were discussed in class,
+	Cory implemented rollback for when an error is raised.
 '''
 try:
     add_employee = Employee( \
@@ -62,21 +67,34 @@ try:
 except SQLAlchemyError as e:
     session.rollback()
     print(f"Transaction rolled back due to error: {e}")
-    print(f"\nThis block would be reached if the insertion was already completed.\n\n")
+    print(f"\n" "This block would be reached if the insertion was already completed." "\n\n")
+
 
 '''
-I lost my job as an Evaluation Assistant because the course instructor
-    no longer has the enrollment numbers to qualify for having me.
+Demonstrates updating a record.
 '''
-print('Updating an Employee record. This is idempotent and has been done before.\n')
-users = session.query(Employee).filter(Employee.lastName == 'Ung').filter(Employee.firstName == 'Anthony')
-users.update({Employee.jobTitle: 'Research Assistant'})
-session.commit()
+try:
+	print('Updating an Employee record. This is idempotent and has been done before.\n')
+	users = session.query(Employee).filter(Employee.lastName == 'Ung').filter(Employee.firstName == 'Anthony')
+	users.update({Employee.jobTitle: 'Research Assistant'})
+	session.commit()
 
-print('Updated employee record after my job got eliminated:\n')
-users = session.query(Employee).filter(Employee.lastName == 'Ung').filter(Employee.firstName == 'Anthony')
-for row in users.all():
-    print(row.__dict__)
+	print('Updated employee record after my job got eliminated:\n')
+	users = session.query(Employee)\
+                    .filter(Employee.lastName == 'Ung')\
+                    .filter(Employee.firstName == 'Anthony')
+	'''
+        Unfortunately, there is no equivalent to 
+            SELECT * (which grabs the entire row of records matching a WHERE clause).
+            The .all() method selects all records but there is no way for us to
+                control the SELECT clause from SQL Alchemy when we are working with Automap.
+    '''
+	for row in users.all():
+    		print(row.__dict__)
+except SQLAlchemyError as e:
+    session.rollback()
+    print(f"Transaction rolled back due to error: {e}")
+    print(f"\n" "Rollback is not supposed to happen in this case." "\n\n")
 
 # prompt user to close then close session and engine
 yes = input("Press any key to close")
