f3f32c8da717a664ee0e10fc445e99d78268497a
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2025-02-25 12:54:07 -0500

    In insert.py, replaced straight line code

--- Final_Deliverables/insert.py
+++ Final_Deliverables/insert.py
@@ -19,96 +19,87 @@ def DEBUG_PRINT(message):
     print(message)
     yes = input('\n' 'Enter any string to exit.' '\n')
 
-# Checks for the connect script being in the same directory.
-# Separate becasue I do not want bots and scrapers to grab my login credentials from Github.
-try:
-    import connect
-except:
-    DEBUG_PRINT('Error!\n'\
-               'You do not have the appropriate\n'\
-               'connect.py file in the right directory\n')
+'''
+    The code examples have straight-line code with no helper functions.
+    We would not ship straight-line code with no helper functions to a real client
+        because that is very hard to read.
+'''
+class globals:
+    engine = None
+    Base = None
+    session = None
+    Employee = None
 
-# Prepare the engine and session
-engine = connect.connect()
-Base = automap_base()
-Base.prepare(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
 
-# Get the class to table mapping
-Employee = Base.classes.employees
+def init_connection():
+    try:
+        import connect
+    except:
+        DEBUG_PRINT('Error!\n'\
+                'You do not have the appropriate\n'\
+                'connect.py file in the right directory\n')
+
+    # Prepare the engine and session
+    globals.engine = connect.connect()
+    globals.Base = automap_base()
+    globals.Base.prepare(globals.engine)
+    Session = sessionmaker(bind=globals.engine)
+    globals.session = Session()
+
+    # Get the class to table mapping
+    globals.Employee = globals.Base.classes.employees
+
 
 '''
 I delete myself from the table so I can insert myself again.
 '''
-try:
-    print('Deleting Employee so I can insert him again.' '\n')
-    employee = session.query(Employee).filter(Employee.employeeNumber == 1950).first()
-    session.delete(employee)
-    session.commit()
-except SQLAlchemyError as e:
-    print('Target employee does not exist.')
-     
+def delete_record():
+    Employee = globals.Employee
+    session = globals.session
+
+    try:
+        print('Deleting Employee so I can insert him again.' '\n')
+        employee = session.query(Employee).filter(Employee.employeeNumber == 1950).first()
+        session.delete(employee)
+        session.commit()
+    except SQLAlchemyError as e:
+        print('Target employee does not exist.')
+
+    
 '''
 I inserted myself into the table.
 reportsTo is NULLable because the President does not report to anybody.
 In addition to the parts of SQLAlchemy that were discussed in class,
 	Cory implemented rollback for when an error is raised.
 '''
-try:
-    add_employee = Employee( \
-                        employeeNumber = 1950, \
-                        lastName = 'Ung', \
-                        firstName = 'Anthony', \
-                        extension = 'x1950', \
-                        email = 'ungant67@students.rowan.edu', \
-                        officeCode = 4, \
-                        reportsTo = None, \
-                        jobTitle = 'Evaluation Assistant' \
-                    )
-    session.add(add_employee)
-    session.commit()
-except SQLAlchemyError as e:
-    session.rollback()
-    print(f"Transaction rolled back due to error: {e}")
-    print(f"\n" "This block would be reached if the insertion was already completed." "\n\n")
+def insert_record():
+    session = globals.session
+    Employee = globals.Employee
 
-'''
-    Unfortunately, there is no equivalent to SELECT *
-        so I have to unwrap all the fields.
-'''
-try:
-    employee = session.query(Employee.employeeNumber, \
-                              Employee.lastName, \
-                              Employee.firstName, \
-                              Employee.extension, \
-                              Employee.email, \
-                              Employee.officeCode, \
-                              Employee.reportsTo, \
-                              Employee.jobTitle \
-                              ).filter(Employee.employeeNumber == 1950).first()
-    
-    print('Employee after insertion')
-    print(employee)
-except SQLAlchemyError as e:
-    print(e)
-     
+    try:
+        add_employee = Employee( \
+                            employeeNumber = 1950, \
+                            lastName = 'Ung', \
+                            firstName = 'Anthony', \
+                            extension = 'x1950', \
+                            email = 'ungant67@students.rowan.edu', \
+                            officeCode = 4, \
+                            reportsTo = None, \
+                            jobTitle = 'Evaluation Assistant' \
+                        )
+        session.add(add_employee)
+        session.commit()
+    except SQLAlchemyError as e:
+        session.rollback()
+        print(f"Transaction rolled back due to error: {e}")
+        print(f"\n" "This block would be reached if the insertion was already completed." "\n\n")
 
-'''
-Demonstrates updating a record.
-'''
-try:
-	users = session.query(Employee).filter(Employee.lastName == 'Ung').filter(Employee.firstName == 'Anthony')
-	users.update({Employee.jobTitle: 'Research Assistant'})
-	session.commit()
-except SQLAlchemyError as e:
-    session.rollback()
-    print(f"Transaction rolled back due to error: {e}")
-    print(f"\n" "Rollback is not supposed to happen in this case." "\n\n")
-
-try:
-    print('\n' 'Updated employee record after my job got eliminated:')
-    employee = session.query(Employee.employeeNumber, \
+    '''
+        Unfortunately, there is no equivalent to SELECT *
+            so I have to unwrap all the fields.
+    '''
+    try:
+        employee = session.query(Employee.employeeNumber, \
                                 Employee.lastName, \
                                 Employee.firstName, \
                                 Employee.extension, \
@@ -117,15 +108,63 @@ try:
                                 Employee.reportsTo, \
                                 Employee.jobTitle \
                                 ).filter(Employee.employeeNumber == 1950).first()
-    print(employee)
-except SQLAlchemyError as e:
-    session.rollback()
-    print(f"Transaction rolled back due to error: {e}")
-    print(f"\n" "Rollback is not supposed to happen in this case." "\n\n")
+        
+        print('Employee after insertion')
+        print(employee)
+    except SQLAlchemyError as e:
+        print(e)
+
+
+'''
+    Demonstrates updating a record.
+'''   
+def update_record():
+    session = globals.session
+    Employee = globals.Employee
+
+    try:
+        users = session.query(Employee).filter(Employee.lastName == 'Ung').filter(Employee.firstName == 'Anthony')
+        users.update({Employee.jobTitle: 'Research Assistant'})
+        session.commit()
+    except SQLAlchemyError as e:
+        session.rollback()
+        print(f"Transaction rolled back due to error: {e}")
+        print(f"\n" "Rollback is not supposed to happen in this case." "\n\n")
+
+    try:
+        print('\n' 'Updated employee record after my job got eliminated:')
+        employee = session.query(Employee.employeeNumber, \
+                                    Employee.lastName, \
+                                    Employee.firstName, \
+                                    Employee.extension, \
+                                    Employee.email, \
+                                    Employee.officeCode, \
+                                    Employee.reportsTo, \
+                                    Employee.jobTitle \
+                                    ).filter(Employee.employeeNumber == 1950).first()
+        print(employee)
+    except SQLAlchemyError as e:
+        session.rollback()
+        print(f"Transaction rolled back due to error: {e}")
+        print(f"\n" "Rollback is not supposed to happen in this case." "\n\n")
+
 
 # prompt user to close then close session and engine
-yes = input('\n' "Press any key to close")
+def close_connection():
+    engine = globals.engine
+    session = globals.session
 
-#cleanup
-session.close()
-engine.dispose()
+    yes = input('\n' "Press any key to close")
+
+    #cleanup
+    session.close()
+    engine.dispose()
+
+def run():
+    init_connection()
+    delete_record()
+    insert_record()
+    update_record()
+    close_connection()
+
+run()
